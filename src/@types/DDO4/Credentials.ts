530ae5cf7e484a5a275ccd58d57555192e245ed7
giurgiur99 <giurgiur99@gmail.com>
2025-04-22 13:13:08 +0300

    integrate-credentials-changes

--- src/@types/DDO4/Credentials.ts
+++ src/@types/DDO4/Credentials.ts
@@ -1,9 +1,24 @@
-export interface Credential {
-  type: string;
-  values: string[];
+export enum CREDENTIALS_TYPES {
+  ADDRESS = 'address',
+  ACCESS_LIST = 'accessList',
+  POLICY_SERVER_SPECIFIC = 'PS-specific Type' // externally handled type
 }
 
+export const KNOWN_CREDENTIALS_TYPES = [
+  CREDENTIALS_TYPES.ADDRESS,
+  CREDENTIALS_TYPES.ACCESS_LIST
+];
+
+export interface Credential {
+  type?: CREDENTIALS_TYPES;
+  values?: string[];
+}
+
+export type MATCH_RULES = 'any' | 'all';
+
 export interface Credentials {
-  allow: Credential[];
-  deny: Credential[];
+  match_allow?: MATCH_RULES; // any =>  it's enough to have one rule matched, all => all allow rules should match, default: 'all'
+  match_deny?: MATCH_RULES; // same pattern as above, default is 'any'
+  allow?: Credential[];
+  deny?: Credential[];
 }
